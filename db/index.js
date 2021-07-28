d14fbcf3ed3bd1c5db97ba4eaea6b13b9d761871
ElegantMrS <josephjumber@gmail.com>
2021-07-28 00:40:29 -0400

    init_db working

--- db/index.js
+++ db/index.js
@@ -4,10 +4,177 @@ const DB_NAME = 'grace_shopper_testing'
 const DB_URL = process.env.DATABASE_URL || `postgres://localhost:5432/${ DB_NAME }`;
 const client = new Client(DB_URL);
 
-// database methods
+//********************************************************** DATABASE METHODS **********************************************************/
+
+//********************************************************** merchandise methods **********************************************************/
+
+async function getAllMerchandise() {
+  try {
+      const { rows: products } = await client.query(`
+      SELECT * FROM merchandise;
+      `)
+
+      return products;
+
+  } catch (error) {
+      throw error;
+  }
+}
+
+async function getMerchandiseByCategory(category) {
+  try {
+      const { rows: products } = await client.query(`
+      SELECT * FROM merchandise
+      WHERE cats = $1;
+      `, [category])
+
+      return products;
+      
+  } catch (error) {
+      throw error;
+  }
+}
+
+async function getMerchandiseByName(merchName) {
+  const { rows: [merchandise] } = await client.query(`
+      SELECT * FROM merchandise
+      WHERE name LIKE $1;
+  `, [merchName]);
+
+  return merchandise;
+}
+
+async function searchMerchandise(searchTerm, category = '') {
+  console.log('Entered client.searchMerchandise', searchTerm, category);
+
+  const catSearch = category.length ? `"Catname"='${category}' AND ` : '';
+
+  console.log('category:', catSearch);
+  try {
+
+      const { rows: merchandise } = await client.query(`
+          SELECT * FROM merchandise
+          JOIN categories ON merchandise.cats=categories.cat_id
+          WHERE ${catSearch} (name LIKE '%${searchTerm}%' OR description LIKE '%${searchTerm}%');
+          
+      `);
+
+      console.log('Successfully retrieved search!', merchandise);
+
+      return merchandise;
+
+  } catch (error) {
+      throw error;
+  }
+
+}
+
+async function getMerchandiseById(merchId) {
+  try {
+      const { rows: [merchandise] } = await client.query(`
+          SELECT * FROM merchandise
+          WHERE merch_id = $1;
+      `, [merchId]);
+
+      const { rows: [reviews] } = await client.query(`
+          SELECT * FROM reviews
+          WHERE "merchId" = $1;
+      `, [merchId])
+
+      merchandise.reviews = reviews;
+
+      return merchandise;
+  } catch (error) {
+      throw error;
+  }
+
+}
+
+async function getMerchandiseByCategory(catId) {
+  const { rows: merchandise } = await client.query(`
+          SELECT * FROM merchandise
+          WHERE cats = $1;
+      `, [catId]);
+
+  return merchandise;
+}
+
+async function updateMerchandise(itemId, fields = {}) {
+  const queryString = Object.keys(fields).map((key, index) => {
+      return `"${key}"=$${index + 1}`
+  }).join(',');
+
+  const { rows: [item] } = await client.query(`
+      UPDATE merchandise
+      SET ${queryString}
+      WHERE merch_id = ${itemId}
+      RETURNING *;
+      `, Object.values(fields));
+}
+
+async function addCategory(name) {
+  try {
+
+      console.log('entered addCategory with cat: ', name);
+      const { rows: [category] } = await client.query(`
+          INSERT INTO categories("Catname")
+          VALUES('${name}')
+          RETURNING *;
+          `);
+
+      return category;
+
+  } catch (error) {
+      throw error;
+  }
+}
+
+async function createMerchandise({ img, name, artist, price, rating = 5, cat }) {
+
+  try {
+
+      console.log('Entered db createMerchandise');
+      const { rows: [merchandise] } = await client.query(`
+          INSERT INTO merchandise(img_url, name, artist, price, rating, cats)
+          VALUES($1, $2, $3, $4, $5, $6)
+          RETURNING *;
+      `, [img, name, artist, price, rating, cat]);
+      console.log('Successfully created merchandise');
+      return merchandise;
+  } catch (error) {
+      throw error;
+  }
+
+}
+
+async function deleteMerchandise(merchId) {
+  try {
+      const { rows: [merchandise] } = await client.query(`
+          DELETE FROM merchandise
+          WHERE merch_id=${merchId}
+          RETURNING *;
+          `)
+
+      return merchandise;
+
+  } catch (error) {
+      throw error;
+  }
+}
 
 // export
 module.exports = {
   client,
+  getAllMerchandise,
+  getMerchandiseByCategory,
+  getMerchandiseById,
+  getMerchandiseByName,
+  createMerchandise,
+  updateMerchandise,
+  addCategory,
+  deleteMerchandise,
+  addCategory,
+  updateMerchandise,
+  searchMerchandise
   // db methods
 }
--- db/init_db.js
+++ db/init_db.js
@@ -1,6 +1,15 @@
 // code to build and initialize DB goes here
 const {
-  client
+  client,
+  getAllMerchandise,
+  getMerchandiseByCategory,
+  getMerchandiseById,
+  getMerchandiseByName,
+  createMerchandise,
+  deleteMerchandise,
+  addCategory,
+  updateMerchandise,
+  searchMerchandise
   // other db methods 
 } = require('./index');
 
@@ -162,15 +171,55 @@ async function buildTables() {
   }
 }
 
-async function populateInitialData() {
+async function createInitialMerchandise() {
   try {
-    // create useful starting data
+    console.log('Starting to create merchandise...');
+
+    const merchandiseToCreate = [
+      { img: `https://chairish-prod.freetls.fastly.net/image/product/sized/5d247484-3849-470a-98f2-ecb1e72bdc56/1943-original-picasso-femme-dans-un-fauteuil-lithograph-7268?aspect=fit&width=1600&height=1600`, name: 'Femme Dans Un Fauteuil', artist: '1943 Original Picasso Lithograph.', price: '1025', rating: '10', cat: 'Cubism' },
+      { img: `https://www.tate.org.uk/art/images/work/T/T05/T05010_10.jpg`, name: 'Weeping Woman', artist: 'Old Master Signed Picasso 1962', price: '2542', rating: '10', cat: 'Cubism' },
+      { img: `http://paintingandframe.com/art-imgs/juan_gris/figure_of_a_woman-6178.jpg`, name: 'Figure of a Woman', artist: 'Juan Gris.', price: '132', rating: '8', cat: 'Cubism' },
+      { img: `https://i.imgur.com/55cRyMW.jpg`, name: 'Three Women', artist: 'Fernand Leger', price: '108', rating: '6', cat: 'Cubism' },
+      { img: `https://i.imgur.com/vZnJhWj.jpg`, name: 'The Knife Grinder', artist: 'Kazimir Severinovich Malevich', price: '101', rating: '10', cat: 'Cubism' },
+      { img: `https://i.imgur.com/fVJMWeU.jpg`, name: 'Symphony', artist: 'Michail Menkov', price: '101', rating: '9', cat: 'Cubism' },
+      { img: `https://i.imgur.com/ANt7SdY.jpg`, name: 'The Confetti Garden', artist: 'Jinlu', price: '1976', rating: '8', cat: 'Impressionism' },
+      { img: `https://i.imgur.com/3dzVMcH.jpg`, name: 'Impressionist Lake Scene', artist: 'John Clymer', price: '975', rating: '10', cat: 'Impressionism' },
+      { img: `https://i.imgur.com/XMLtXHc.jpg`, name: 'Impressionist French Landscape', artist: 'Unknown', price: '750', rating: '7', cat: 'Impressionism' },
+      { img: `https://i.imgur.com/e6VBJ35.jpg`, name: 'Spring in Central Park', artist: 'Natasha Kramskaya', price: '725', rating: '10', cat: 'Impressionism' },
+      { img: `https://i.imgur.com/XTUfn8P.jpg`, name: 'Signed Original Street Scene Paris', artist: 'Unknown', price: '695', rating: '9', cat: 'Impressionism' },
+      { img: `https://i.imgur.com/QrkyVTP.jpg`, name: 'Floral Landscape Flower Fields IV', artist: 'Michael Budden', price: '600', rating: '10', cat: 'Impressionism' },
+      { img: `https://ik.imagekit.io/theartling/p/products/Product/97702d17a693459189d57a139990de2c.png`, name: 'Cash Owns Everything Around Me', artist: 'Resatio Adi Putra', price: '250', rating: '10', cat: 'Popart' },
+      { img: `https://ik.imagekit.io/theartling/p/products/Product/9101370e619244749ddeb8fe237247ce.jpg?tr=cm-pad_resize,w-1072,h-1072,bg-FFFFFF`, name: 'Massive Stars Collapse III', artist: 'Resatio Adi Putra', price: '58', rating: '8', cat: 'Popart' },
+      { img: `https://ik.imagekit.io/theartling/p/products/Product/1cab7371a0da4759b29a83645d899690.jpg?tr=cm-pad_resize,w-1072,h-1072,bg-FFFFFF`, name: 'Piss Off', artist: 'Jirapat Tatsanasomboon', price: '13000', rating: '10', cat: 'Popart' },
+      { img: `https://ik.imagekit.io/theartling/p/products/Product/bd346e519f8841d29e844463e272ca58.jpeg?tr=cm-pad_resize,w-1072,h-1072,bg-FFFFFF`, name: 'Desire Ingrained', artist: 'Jirapat Tatsanasomboon', price: '13000', rating: '9', cat: 'Popart' },
+      { img: `https://i.pinimg.com/originals/a8/a3/04/a8a3047e0985825ff3c30efd1acfeda1.png`, name: 'Silaturahmi Gorilla Glass', artist: 'Hendra Hehe Harsono', price: '3000', rating: '7', cat: 'Popart' },
+      { img: `https://ik.imagekit.io/theartling/p/artworks/Hendra_Hehe_Harsono_CapitalNoise.jpg?tr=cm-pad_resize,w-1072,h-1072,bg-FFFFFF`, name: 'Capital Noise', artist: 'Hendra Hehe Harsono', price: '5000', rating: '10', cat: 'Popart' },
+      { img: `https://i.imgur.com/qRwj1Sq.jpg`, name: 'The River Seine', artist: 'Adolph Clary Baroux', price: '28500', rating: '9', cat: 'PostImpressionalism' },
+      { img: `https://i.imgur.com/cfLkUpf.jpg`, name: 'Ballerinas Paris Opera', artist: 'Jean Louis Marcel Cosson', price: '13605', rating: '10', cat: 'PostImpressionalism' },
+      { img: `https://i.imgur.com/wZ0kJ65.jpg`, name: 'Early Morning Hyde Park London', artist: 'Elliott Seabrooke', price: '7396', rating: '7', cat: 'PostImpressionalism' },
+      { img: `https://i.imgur.com/3nB1NrO.jpg`, name: 'Cafe Porto Fino Italy', artist: 'Forrest Hewit', price: '5973', rating: '10', cat: 'PostImpressionalism' },
+      { img: `https://i.imgur.com/cEwoIwJ.jpg`, name: 'Village in Mexico', artist: 'Jacques Zucker', price: '5250', rating: '9', cat: 'PostImpressionalism' },
+      { img: `https://i.imgur.com/rgROuVZ.jpg`, name: 'Rural Farm Scene', artist: 'Ludvig Jaconbsen', price: '4800', rating: '10', cat: 'PostImpressionalism' },
+      { img: `https://i.imgur.com/NTgAzLHh.jpg`, name: 'True Story', artist: 'A Contemporary Artpiece.', price: '675', rating: '10', cat: 'Contemporary' },
+      { img: `https://i.imgur.com/8WWncJM.jpg`, name: 'Celebration', artist: 'A Contemporary Artpiece.', price: '649', rating: '6', cat: 'Contemporary' },
+      { img: `https://i.imgur.com/hAYx7R8.jpg`, name: 'Envisioning', artist: 'A Contemporary Artpiece.', price: '659', rating: '8', cat: 'Contemporary' },
+      { img: `https://i.imgur.com/cZOxU1b.jpg`, name: 'Safe and Sound', artist: 'A Contemporary Artpiece.', price: '685', rating: '5', cat: 'Contemporary' },
+      { img: `https://i.imgur.com/WZNpYSy.jpg`, name: 'Zone 1', artist: 'A Contemporary Artpiece.', price: '166', rating: '9', cat: 'Contemporary' },
+      { img: `https://i.imgur.com/JC3k8yp.jpg`, name: `You Can't Keep Spring From Coming`, artist: 'A Contemporary Artpiece.', price: '399', rating: '7', cat: 'Contemporary' }
+    ]
+    const merchandise = await Promise.all(merchandiseToCreate.map(createMerchandise));
+
+    console.log('merchandise created:');
+    console.log(merchandise);
+
+    console.log('Finished creating merchandise!');
   } catch (error) {
+    console.error('Error creating merchandise!');
     throw error;
   }
 }
 
 buildTables()
-  .then(populateInitialData)
+  .then(createInitialMerchandise)
   .catch(console.error)
   .finally(() => client.end());
